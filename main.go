eb774ab20c135eec653348d4bca8f574bb5e89a3
Shrey Bhardwaj <bhardwajshrey10@gmail.com>
2023-09-23 18:21:40 +0530

    repl and run from file setup

--- main.go
+++ main.go
@@ -1,9 +1,29 @@
 package main
 
 import (
+    "bufio"
     "fmt"
     "os"
+    "os/user"
 )
+
+const LOGO = `
+
+ ██▓     ▒█████  ▒██   ██▒
+▓██▒    ▒██▒  ██▒▒▒ █ █ ▒░
+▒██░    ▒██░  ██▒░░  █   ░
+▒██░    ▒██   ██░ ░ █ █ ▒ 
+░██████▒░ ████▓▒░▒██▒ ▒██▒
+░ ▒░▓  ░░ ▒░▒░▒░ ▒▒ ░ ░▓ ░
+░ ░ ▒  ░  ░ ▒ ▒░ ░░   ░▒ ░
+  ░ ░   ░ ░ ░ ▒   ░    ░  
+    ░  ░    ░ ░   ░    ░  
+                          
+
+`
+
+
+const prompt = "> "
 
 func main() {
     args := os.Args[1:]
@@ -20,13 +40,41 @@ func main() {
 
 func runFile(path string) {
     fmt.Printf("Running on %s...\n", path)
-    // actually do something
+    data, err := os.ReadFile(path)
+    if err != nil {
+        fmt.Fprintf(os.Stderr, "Filepath '%s' specified wrong. Please re-check...\n", path)
+        os.Exit(1)
+    }
+    run(string(data))
 }
 
 func runPrompt() {
-    // this is the REPL
-    fmt.Printf("Welcome to Lox. Start typing away...\n")
-    fmt.Printf("> ")
-    fmt.Printf("\n")
-    // do something here as well
+    user, err := user.Current()
+    if err != nil {
+        panic(err)
+    }
+
+    fmt.Print(LOGO)
+    fmt.Printf("Hello %s, welcome to Lox...\n\n", user.Username)
+    
+    scanner := bufio.NewScanner(os.Stdin)
+
+    for {
+        fmt.Printf(prompt)
+        isScanned := scanner.Scan()
+        if !isScanned {
+            // error while scanning tokens or EOF. stdin is however an always open file so no EOF unless Ctrl^C
+            return
+        }
+
+        line := scanner.Text()  // returns most recent token scanned. Token here is a string of bytes (the line of code you just typed)
+        if line == "" {
+            break
+        }
+        run(line)
+    }
+}
+
+func run(source string) {
+    fmt.Printf("%s\n", source)
 }
